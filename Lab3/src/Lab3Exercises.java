1977302e1d42a31ac232f17af67e40a8392e2276
4010a6 <4010a6@carambola.ece.uprm.edu>
2017-11-15 12:26:56 -0400

    Added background color and two borders with different colors

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -3,9 +3,12 @@ import javax.swing.JFrame;
 public class Lab3Exercises {
 	public static void main(String[] args) {
 		JFrame myFrame = new JFrame("Cesar Justiniano");
+		MyPanelClass myPanel = new MyPanelClass();
+        myFrame.getContentPane().add(myPanel);
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(900, 400);
 		myFrame.setSize(200, 200);
+		
 		myFrame.setVisible(true);
 	}
 }
